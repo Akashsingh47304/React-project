2428764fb8a9d0336e76be54d5bdd12df1f9a35c
Akashsingh47304 <bhadoriaakash3@gmail.com>
2026-06-14 23:08:53 +0530

    changes in the reactRouter project

--- reactRouter/src/App.jsx
+++ reactRouter/src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
 
 import './App.css'
+import Header from './component/Header/Header'
+import Footer from './component/Footer/Footer'
+import { Outlet } from 'react-router-dom'
 
 function App() {
  
 
   return (
     <>
-    
+    <Header />
+      <Outlet />
+    <Footer />
     </>
   )
 }
--- reactRouter/src/main.jsx
+++ reactRouter/src/main.jsx
@@ -1,10 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { Route, RouterProvider,createBrowserRouter,createRoutesFromElements } from 'react-router-dom'
 import App from './App.jsx'
+import Home from './component/Home/Home.jsx'
+import About from './component/About/About.jsx'
+import Contact from './component/Contact/Contact.jsx'
+import './index.css'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path ="contact-us" element={<Contact />} />
+    </Route>
+  )
+);
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
