925684c30940c5bbcd7f302989d59c25b566a422
Akashsingh47304 <bhadoriaakash3@gmail.com>
2026-06-16 17:34:34 +0530

    worked on the functionality of form and todolists

---
+++ Todo/src/context/todoContext.js
@@ -0,0 +1,18 @@
+import {createContext, useContext} from "react";
+
+export const TodoContext = createContext({
+    todos:[
+        {
+            id:1,
+            todo:"Learn React",
+            completed:false
+        }
+       
+    ],
+     addTodo:()=>{},
+        updateTodo:()=>{},
+        deleteTodo:()=>{},
+        toggleTodoComplete:()=>{}
+});
+export const useTodo = () => useContext(TodoContext);
+export const TodoProvider=TodoContext.Provider;
